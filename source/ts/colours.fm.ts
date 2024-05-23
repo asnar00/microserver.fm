574b36d647805fc97e3de823d91ac8fc1a0b2151
asnar00 <ash.nehru@gmail.com>
2024-05-23 17:07:30 +0100

    colours

--- source/ts/colours.fm.ts
+++ source/ts/colours.fm.ts
@@ -14,16 +14,22 @@ interface Colour {
     @on colour(r: number=0, g: number=0, b: number=0): Colour {
         return {r, g, b} as Colour;
     }
+    @on add_colours(c1: Colour, c2: Colour): Colour {
+        return colour(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b);
+    }
 }
 
 interface Colour {
     a: number;
 }
-declare const colour: (r: number, g: number, b: number, a?:number) => Colour;
+declare const colour: (r?: number, g?: number, b?: number, a?:number) => Colour;
 
 @feature class _ColourAlpha extends _Colour {
     @on colour(r: number=0, g: number=0, b: number=0, a: number=1): Colour {
         return {r, g, b, a} as Colour;
+    }
+    @on add_colours(c1: Colour, c2: Colour): Colour {
+        return colour(c1.r + c2.r, c1.g + c2.g, c1.b + c2.b, c1.a + c2.a);
     }
 }
 
